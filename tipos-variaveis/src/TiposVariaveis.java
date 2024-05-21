d270fe137d847c7f5debd1f62c8e088185b1b3c4
EmersonW Silva <emrsnwslv@gmail.com>
2024-05-21 17:17:00 -0300

    tipos de variaveis concluido

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+      String meuNome = "Emerson W. Silva";
+
+      double salarioMinimo = 2500;
+
+      short numeroCurto = 1;
+      int numeroNormal = numeroCurto;
+      short numeroCurto2 = (short) numeroNormal;
+
+      int numero = 5;
+
+      numero = 10;
+
+      System.out.print(numero);
+
+      final double PI = 3.14;
     }
 }
